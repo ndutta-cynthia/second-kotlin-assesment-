be19e7807589d4c95c47295369c1e4150dc93cea
ndutta-cynthia <nduttamaina@gmail.com>
2024-03-25 08:27:13 +0300

    added assessment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+fun main(){
+println(volume(5.0 , 3.1459))
+    allnums<Int>(2, 10.0,7)
+    println(allnums.sum)
+}
+
+fun givenStr(sentence:String) {
+    val stnc= "Barnie bakes brown bangels and burns"
+    println( stnc.replace("b", ""))
+    println(stnc)
+
+
+}
+
+fun <Array> allnums (num1: Int, num2 :Double, num3 :Int) {
+    return (allnums<Int>(allnums.toSum))
+
+
+}
+fun volume(radius: Double, pie: Double): Double {
+var volume= 4/3*pie*radius
+    return(volume)
+
+}
+
+fun palindrome( word:String): Boolean{
+    var words= <ArrayOf> ("madam","wow","kayak")
+    println(True) {
+        if word is palidrome
+    }
+    else{
+        println(false)
+    }
+}
+
